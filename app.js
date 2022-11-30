cc67d942aaa960f1b4dd5a8de2caa82d82b9ba14
Henry <henryppercy@gmail.com>
2022-11-30 10:52:27 +0000

    Set up node app and entry point

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const routes = require('./config/routes');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+routes(app);
+
+app.listen(port, () => {
+    console.log(`Server running at port: ${port}`)
+});
