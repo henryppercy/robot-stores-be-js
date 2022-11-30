480cb5f3502183b98a48d840223043e4cdce8ebe
Henry <henryppercy@gmail.com>
2022-11-30 11:44:00 +0000

    Created controller for individual product

--- config/routes.js
+++ config/routes.js
@@ -2,7 +2,7 @@ const productsController = require('../controllers/productsController');
 
 const routes = (app) => {
     app.get('/products', productsController.getProducts)
-    // app.get('/products:id', productsController.getProduct)
+    app.get('/products/:id', productsController.getProduct)
 }
 
 module.exports = routes;
--- controllers/productsController.js
+++ controllers/productsController.js
@@ -6,4 +6,12 @@ const getProducts = (req, res) => {
         .then((products) => res.json(products));
 }
 
+const getProduct = (req, res) => {
+    console.log('Products controller: getProduct');
+    let id = req.params.id;
+    productsService.getProduct(id)
+        .then((product) => res.json(product));
+}
+
 module.exports.getProducts = getProducts;
+module.exports.getProduct = getProduct;
