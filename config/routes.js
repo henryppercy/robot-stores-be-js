eb2f2136678d517e3abc9eb8c004314c85156f57
Henry <henryppercy@gmail.com>
2022-11-30 10:52:56 +0000

    Set up route for products page

---
+++ config/routes.js
@@ -0,0 +1,8 @@
+const productsController = require('../controllers/productsController');
+
+const routes = (app) => {
+    app.get('/products', productsController.getProducts)
+    // app.get('/products:id', productsController.getProduct)
+}
+
+module.exports = routes;
