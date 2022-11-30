d8c7f9be308cd95137f25e57399afab9240cc291
Henry <henryppercy@gmail.com>
2022-11-30 11:59:20 +0000

    Created service function for individual product

--- services/productsService.js
+++ services/productsService.js
@@ -5,4 +5,10 @@ const getProducts = async () => {
     return await productsRepository.getProducts();
 }
 
+const getProduct = async (id) => {
+    console.log('Service: getProduct ' + id);
+    return await productsRepository.getProduct(id);
+}
+
 module.exports.getProducts = getProducts;
+module.exports.getProduct = getProduct;
